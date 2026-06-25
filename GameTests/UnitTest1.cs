081094438d813f06fba672e7c8bc27eae6e5c206
JabloZ <jablozkontakt@gmail.com>
2026-06-26 01:31:05 +0200

    dodanie testow (zniknely z jakiegos powodu)

--- GameTests/UnitTest1.cs
+++ GameTests/UnitTest1.cs
@@ -1,1 +1,45 @@
-﻿
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+public class ScoreDbTests
+{
+    private string NewTestDbPath() => Path.Combine(Path.GetTempPath(), $"test_{Guid.NewGuid()}.db");
+
+    [Fact]
+    public void SavingScore_AddsOneRecord()
+    {
+        var db = new GameDb(NewTestDbPath());
+        db.Database.EnsureCreated();
+
+        db.Scores.Add(new ScoreEntry { Value = 42, ScoreDate = DateTime.Now });
+        db.SaveChanges();
+
+        Assert.Equal(1, db.Scores.Count());
+    }
+
+    [Fact]
+    public void GetTopScores_ReturnsHighestFirst()
+    {
+        var db = new GameDb(NewTestDbPath());
+        db.Database.EnsureCreated();
+
+        db.Scores.Add(new ScoreEntry { Value = 10, ScoreDate = DateTime.Now });
+        db.Scores.Add(new ScoreEntry { Value = 99, ScoreDate = DateTime.Now });
+        db.SaveChanges();
+
+        var top = db.Scores.OrderByDescending(s => s.Value).First();
+
+        Assert.Equal(99, top.Value);
+    }
+
+    [Fact]
+    public void EmptyDatabase_HasNoScores()
+    {
+        var db = new GameDb(NewTestDbPath());
+        db.Database.EnsureCreated();
+
+        Assert.Empty(db.Scores);
+    }
+}
